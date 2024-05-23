42ebbc0f9bced5f3858435034471257bdc20f317
MarinaOfficiall <marina.pershina.2110@list.ru>
2024-05-23 18:04:41 +0300

    Add files via upload

---
+++ ONASForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fashion_House
+{
+    public partial class ONASForm : Form
+    {
+        public ONASForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Glavbutton2_Click(object sender, EventArgs e)
+        {
+            FormGlav f121 = new FormGlav();
+            this.Close();
+            f121.Show();
+        }
+    }
+}
---
+++ VKLADKIForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Fashion_House
+{
+    public partial class VKLADKIForm : Form
+    {
+        //Database database= new Database();
+        public VKLADKIForm()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GLaVbutton_Click(object sender, EventArgs e)
+        {
+            FormGlav f121 = new FormGlav();
+            this.Close();
+            f121.Show();
+        }
+
+        private void VKLADKIForm_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "_FASHION_HOUSEDataSet1.Agency". При необходимости она может быть перемещена или удалена.
+            this.agencyTableAdapter.Fill(this._FASHION_HOUSEDataSet1.Agency);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "_FASHION_HOUSEDataSet.Agents". При необходимости она может быть перемещена или удалена.
+            this.agentsTableAdapter.Fill(this._FASHION_HOUSEDataSet.Agents);
+
+        }
+    }
+}
